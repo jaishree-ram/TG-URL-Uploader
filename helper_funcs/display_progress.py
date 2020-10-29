3cacb3e77d1e30d47211d6ba58d015c7703df610
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2020-10-29 18:34:06 +0530

    Styling 🔥

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -42,12 +42,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \nP: {2}%\n".format(
-            ''.join(["█" for i in range(math.floor(percentage / 5))]),
-            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
+        progress = "[{0}{1}] \nProgress😉: {2}%\n".format(
+            ''.join(["▦" for i in range(math.floor(percentage / 5))]),
+            ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+        tmp = progress + "Done✔️:〘{0}〙\nTotal🙀 :〘{1}〙\nSpeed Detected :〘6G〙 \nSpeed🚀:〘{2}/s〙\nTimeLeft🙄:〘{3}〙\n© @KCHDMOVIES2019\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
